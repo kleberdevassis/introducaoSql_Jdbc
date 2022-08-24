81399534c7696b562887dbe1c72169a1e5103d44
vanes <vanes@LAPTOP-DG4J0EB8>
2022-08-24 15:00:22 -0300

    toString com erro

--- pos-java-jdbc/src/main/java/dao/UserPosDao.java
+++ pos-java-jdbc/src/main/java/dao/UserPosDao.java
@@ -2,7 +2,10 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import ConexaoJdbc.SingleConnection;
 import model.Userposjava;
@@ -36,7 +39,26 @@ public class UserPosDao {
 		}
 	}
 	
-	
+	public List<Userposjava> listar() throws Exception{
+		List<Userposjava> list = new ArrayList<>();
+		
+		String sql = "select * from userposjava";
+		
+		PreparedStatement statement = connection.prepareStatement(sql);
+		
+		ResultSet resultado = statement.executeQuery();
+		
+		while(resultado.next()) {
+			Userposjava userposjava = new Userposjava();
+			userposjava.setId(resultado.getLong("id"));
+			userposjava.setNome(resultado.getString("nome"));
+			userposjava.setEmail(resultado.getString("email"));
+			
+			list.add(userposjava);
+		}
+		
+		return list;
+	}
 	
 	
 
--- pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
+++ pos-java-jdbc/src/test/java/pos_java_jdbc/pos_java_jdbc/TesteBancoJdbc.java
@@ -1,25 +1,44 @@
 package pos_java_jdbc.pos_java_jdbc;
 
+import java.util.List;
+
 import org.junit.Test;
 
 import ConexaoJdbc.SingleConnection;
 import dao.UserPosDao;
 import model.Userposjava;
 
-public class TesteBancoJdbc{
-	
-	
+public class TesteBancoJdbc {
+
 	@Test
 	public void initBanco() {
 		UserPosDao userPosDao = new UserPosDao();
 		Userposjava userposjava = new Userposjava();
-		
+
 		userposjava.setId(8L);
 		userposjava.setNome("Eduardo");
 		userposjava.setEmail("eduardo@gmail.com");
-		
+
 		userPosDao.salvar(userposjava);
-		
+
+	}
+
+	@Test
+	public void initListar() {
+		UserPosDao dao = new UserPosDao();
+		try {
+			List<Userposjava> list = dao.listar();
+
+			for (Userposjava userposjava : list) {
+				System.out.println(userposjava);
+				System.out.println("-------------------------");
+			}
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 	}
 
 }
