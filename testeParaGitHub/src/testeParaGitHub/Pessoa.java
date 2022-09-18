9d6b5882b83fa918f15d9e509ea45a6a291cbb90
vanes <vanes@LAPTOP-DG4J0EB8>
2022-09-17 21:41:48 -0300

    atualizaçao

---
+++ testeParaGitHub/src/testeParaGitHub/Pessoa.java
@@ -0,0 +1,33 @@
+package testeParaGitHub;
+
+public class Pessoa {
+
+	
+	private String nome = "kleber";
+	private int idade = 55;
+	private int cpf = 23412342;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public int getCpf() {
+		return cpf;
+	}
+	public void setCpf(int cpf) {
+		this.cpf = cpf;
+	}
+	
+	
+	
+	
+	
+	
+}
